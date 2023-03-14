12d4d106585643d9893200e51d73549ca3ed9dee
SenaAlexander <alexsena82@gmail.com>
2023-03-14 20:21:47 -0300

    Criei uma calculadora em java

---
+++ app/src/main/java/calculadora_java/Calculadora.java
@@ -0,0 +1,25 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package calculadora_java;
+
+/**
+ *
+ * @author Admin
+ */
+public class Calculadora {
+    public double somar(double a, double b){
+        return a + b;
+    }
+    
+     public double subtrair(double a, double b){
+        return a - b;
+    }
+      public double multiplicar(double a, double b){
+        return a * b;
+    }
+       public double dividir(double a, double b){
+        return a / b;
+    }
+}
